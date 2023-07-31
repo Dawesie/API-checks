556cb00e8e85c4fda5e28c8817df53ee7c17981f
Dawesie <marie.dawe01967@gmail.com>
2023-07-31 18:02:10 +0000

    added JS code to get request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-egFYYnJVGLAaZ9666Ru19_VJoX8
+const API_KEY = "egFYYnJVGLAaZ9666Ru19_VJoX8";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?apr_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+
+
+}
